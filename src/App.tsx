6b5de480757ff01e4df50eedc9f6c9aa39dbd9c9
cleydsonoliveira <cleydsonoliveiraips392@gmail.com>
2022-07-07 15:59:16 -0300

    Add estilos base do topo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import * as C from './App.styles';
+import { useState } from "react";
+import * as C from "./App.styles";
+import { Item } from "./types/Item";
 
 const App = () => {
-  return(
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: "Comprar pao na padaria", done: false },
+    { id: 2, name: "Comprar bolo na padaria", done: false },
+  ]);
+
+  return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+
+        {/* adicionar nova tarefa*/}
+
+        {list.map((item, index) => (
+          <div>{item.name}</div>
+        ))}
       </C.Area>
     </C.Container>
   );
-}
+};
 export default App;
